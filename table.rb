3497c4bee15d047c5114e46cbcb2691cfee16822
tester <dmfrodol@yandex.ru>
2019-12-13 05:23:35 -0800

    add some methods

--- table.rb
+++ table.rb
@@ -4,6 +4,8 @@ require_relative 'player.rb'
 require_relative 'dealer.rb'
 
 class Table
+  SCORES = {}
+
   def initialize
     @players = []
     @dealer = nil
@@ -21,5 +23,50 @@ class Table
       message = "Should be either Player or Dealer! Got = #{person.class}"
       raise BlackjackError, message
     end
+  end
+
+  def start_game
+    validate!
+    construct_deck
+    distribute_initial_cards
+    calculate_scores
+  end
+
+  private
+
+  def validate!
+    validate_players
+    validate_dealer
+  end
+
+  def validate_players
+    message = 'There should be at least one player at a table!'
+    raise BlackjackError, message if @players.empty?
+  end
+
+  def validate_dealer
+    message = 'There should be a dealer at the table!'
+    raise BlackjackError, message unless @dealer
+  end
+
+  def construct_deck
+    @deck = Deck.new
+    @deck.shuffle!
+  end
+
+  def distribute_initial_cards
+    @players[0].add_card(@deck.take_card)
+    @players[0].add_card(@deck.take_card)
+    @dealer.add_card(@deck.take_card)
+    @dealer.add_card(@deck.take_card)
+  end
+
+  def calculate_scores
+    @players[0].increase_score_by(calculate_score(@players[0].hand))
+    @dealer.increase_score_by(calculate_score(@dealer.hand))
+  end
+
+  def calculate_score(cards)
+
   end
 end
