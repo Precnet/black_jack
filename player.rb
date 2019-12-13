ec4bb78fce4ad1ab7b6294bb6f4710f39b23e6ee
Dmitry Davdov <dmfrodol@yandex.ru>
2019-12-13 06:24:46 +0300

    add class structure

--- player.rb
+++ player.rb
@@ -1,1 +1,24 @@
+# Class for playing card representation
 
+class Card
+  def initialize(value, suit)
+    @suit = suit
+    @value = value
+    validate!
+  end
+
+  private
+
+  def validate!
+    validate_suit
+    validate_value
+  end
+
+  def validate_suit
+
+  end
+
+  def validate_value
+
+  end
+end
