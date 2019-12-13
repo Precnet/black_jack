b0daf96983a553e6f6e75a64809d4d057fd1cb11
tester <dmfrodol@yandex.ru>
2019-12-13 03:27:29 -0800

    define basic player functionality

--- player.rb
+++ player.rb
@@ -1,8 +1,35 @@
 # frozen_string_literal: true
 
+require_relative 'blackjack_error.rb'
+
 class Player
-  def initialize
+  ROLES = %I[player dealer].freeze
+
+  def initialize(money = 100, role = :player)
     @hand = []
     @score = 0
+    @money = money
+    validate!
+  end
+
+  private
+
+  def validate!
+    validate_money
+    validate_role
+  end
+
+  def validate_money
+    message = 'Money should be positive integer!'
+    money_correct = @money.is_a?(Integer) && @money.positive?
+    raise BlackjackError, message unless money_correct
+  end
+
+  def validate_role
+    message_type = 'Role should be a symbol!'
+    raise BlackjackError, message_type unless @role.is_a? Symbol
+
+    message_value = "Role should be either ':player' or ':dealer'!"
+    raise BlackjackError, message_value unless ROLES.include? @role
   end
 end
