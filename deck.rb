4d2287d8142fc89d717ca84bd69ae911eac549f1
tester <dmfrodol@yandex.ru>
2019-12-13 02:47:25 -0800

    add deck creation method

--- deck.rb
+++ deck.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
-class Deck
+require_relative 'card.rb'
 
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = create_deck
+  end
+
+  private
+
+  def create_deck
+    deck = []
+    Card.values.each do |value|
+      Card.suits.each_key do |suit|
+        deck.push(value.to_s + Card.suits[suit])
+      end
+    end
+    deck
+  end
 end
