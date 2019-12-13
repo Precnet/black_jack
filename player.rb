16ac39bc4751f2ddb37c89076ae64f3cf6d3cf50
tester <dmfrodol@yandex.ru>
2019-12-13 03:30:52 -0800

    remove dealer stuff from class

--- player.rb
+++ player.rb
@@ -3,9 +3,7 @@
 require_relative 'blackjack_error.rb'
 
 class Player
-  ROLES = %I[player dealer].freeze
-
-  def initialize(money = 100, role = :player)
+  def initialize(money = 100)
     @hand = []
     @score = 0
     @money = money
@@ -16,20 +14,11 @@ class Player
 
   def validate!
     validate_money
-    validate_role
   end
 
   def validate_money
     message = 'Money should be positive integer!'
     money_correct = @money.is_a?(Integer) && @money.positive?
     raise BlackjackError, message unless money_correct
-  end
-
-  def validate_role
-    message_type = 'Role should be a symbol!'
-    raise BlackjackError, message_type unless @role.is_a? Symbol
-
-    message_value = "Role should be either ':player' or ':dealer'!"
-    raise BlackjackError, message_value unless ROLES.include? @role
   end
 end
