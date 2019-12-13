c506e334faf647cec7914f83f39ef013e1cf3b71
tester <dmfrodol@yandex.ru>
2019-12-13 04:40:27 -0800

    add dealer logic

--- dealer.rb
+++ dealer.rb
@@ -1,3 +1,13 @@
 class Dealer < Player
+  def initialize(money = 100)
+    super money
+  end
 
+  def take_turn
+    if @score >= 17
+      :pass_turn
+    else
+      :take_card
+    end
+  end
 end
