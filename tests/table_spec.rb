943743582c35a9533589342153148ff9c19f022b
Dmitry Davdov <dmfrodol@yandex.ru>
2019-12-13 21:58:35 +0300

    add file for Table tests

---
+++ tests/table_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../table.rb'
+
+describe 'Table' do
+  before(:all) do
+    @table = Table.new()
+  end
+  context 'player operation' do
+
+  end
+end
