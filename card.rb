622f35053bc5d85c1c2b746361595015f9a4a835
Dmitry Davdov <dmfrodol@yandex.ru>
2019-12-13 06:46:20 +0300

    move to actual card file

--- card.rb
+++ card.rb
@@ -1,1 +1,40 @@
+# Class for playing card representation
+require_relative 'blackjack_error.rb'
 
+class Card
+  SUIT_LENGTH = 1
+  VALUE_LENGTH = 2
+
+  def initialize(value, suit)
+    @suit = suit
+    @value = value
+    validate!
+  end
+
+  private
+
+  def validate!
+    validate_suit
+    validate_value
+  end
+
+  def validate_suit
+    validate_type(@suit, String)
+    validate_length(@suit, SUIT_LENGTH)
+  end
+
+  def validate_value
+    validate_type(@value, String)
+    validate_length(@value, VALUE_LENGTH)
+  end
+
+  def validate_type(attribute, type)
+    message = "'#{attribute}' should be '#{type}'! Got = '#{attribute.class}"
+    raise BlackjackError, message unless attribute.is_a? type
+  end
+
+  def validate_length(attribute, length)
+    message = "'#{attribute}' should have length between 0 and '#{length}'!"
+    raise BlackjackError, message unless 0 < attribute.length && attribute.length < length
+  end
+end
