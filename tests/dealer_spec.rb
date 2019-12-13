85d4aabf041feb66952d27e57b5ccfbbbc2ba798
tester <dmfrodol@yandex.ru>
2019-12-13 05:11:51 -0800

    add tests for dealer

--- tests/dealer_spec.rb
+++ tests/dealer_spec.rb
@@ -10,7 +10,20 @@ describe 'Dealer' do
   end
   context 'dealer operation' do
     it 'should create dealer objects' do
-
+      expect { Dealer.new(Table.new) }.not_to raise_error
+    end
+  end
+  context 'taking turn' do
+    it 'should correctly process their turn' do
+      expect(@dealer.score).to eq(0)
+      @dealer.increase_score_by(10)
+      expect(@dealer.take_turn).to eq(:take_card)
+      @dealer.increase_score_by(5)
+      expect(@dealer.score).to eq(15)
+      expect(@dealer.take_turn).to eq(:take_card)
+      @dealer.increase_score_by(5)
+      expect(@dealer.score).to eq(20)
+      expect(@dealer.take_turn).to eq(:pass_turn)
     end
   end
 end
