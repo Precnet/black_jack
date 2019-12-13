a909a5b15bfbb7da2da1230561945900e3391cc5
tester <dmfrodol@yandex.ru>
2019-12-13 03:39:19 -0800

    add methods for player

--- player.rb
+++ player.rb
@@ -3,11 +3,32 @@
 require_relative 'blackjack_error.rb'
 
 class Player
+  attr_reader :money, :hand, :score
+
   def initialize(money = 100)
     @hand = []
     @score = 0
     @money = money
     validate!
+  end
+
+  def add_card(card)
+    @hand.push(card)
+  end
+
+  def increase_score_by(value)
+    @score += value
+  end
+
+  def make_bet(value = 10)
+    message = 'Not enough money!'
+    raise BlackjackError, message if (@money - value).negative?
+
+    @money -= value
+  end
+
+  def take_bank(value)
+    @money += value
   end
 
   private
