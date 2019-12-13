0a6ccc80ef2a48ce73783e253016841eb337c3e7
tester <dmfrodol@yandex.ru>
2019-12-13 03:13:30 -0800

    add tests for Decks

--- tests/deck_spec.rb
+++ tests/deck_spec.rb
@@ -4,9 +4,28 @@ require 'rspec'
 require_relative '../deck.rb'
 
 describe 'Deck' do
+  before(:all) do
+    @deck1 = Deck.new
+    @deck2 = Deck.new
+  end
   context 'deck creation' do
     it 'creates deck objects' do
-      deck = Deck.new
+      expect { Deck.new }.not_to raise_error
+      expect(@deck1.cards.length).to eq(52)
+      expect(@deck1.cards.length).to eq(@deck2.cards.length)
+    end
+    it 'shuffles cards in deck' do
+      @deck1.shuffle!
+      expect(@deck1.cards).not_to eq(@deck2.cards)
+    end
+    it 'takes cards for deck' do
+      card = @deck1.take_card
+      expect(@deck1.cards.length).to eq(51)
+      expect(card.class).to be(Card)
+      @deck1.take_card
+      @deck1.take_card
+      @deck1.take_card
+      expect(@deck1.cards.length).to eq(48)
     end
   end
 end
