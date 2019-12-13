1af722645c1d4174f73848ae955635d8eb4989f0
tester <dmfrodol@yandex.ru>
2019-12-13 04:54:15 -0800

    add method for adding persons to a table

--- table.rb
+++ table.rb
@@ -1,2 +1,22 @@
+# frozen_string_literal: true
+
 class Table
+  def initialize
+    @players = []
+    @dealer = nil
+  end
+
+  def add_to_table(person)
+    if person.class.is_a? Player
+      @players.push(person)
+    elsif person.class.is_a? Dealer
+      message = 'There Can Be Only One... dealer!'
+      raise BlackjackError, message if @dealer
+
+      @dealer = person
+    else
+      message = "Should be either Player or Dealer! Got = #{person.class}"
+      raise BlackjackError, message
+    end
+  end
 end
