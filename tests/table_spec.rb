e41084f717d178981f7290f22366b185b53a959b
Dmitry Davdov <dmfrodol@yandex.ru>
2019-12-14 11:45:36 +0300

    add tests

--- tests/table_spec.rb
+++ tests/table_spec.rb
@@ -5,9 +5,18 @@ require_relative '../table.rb'
 
 describe 'Table' do
   before(:all) do
-    @table = Table.new()
+    @table = Table.new
   end
   context 'player operation' do
-
+    it 'should create an empty table' do
+      expect(@table.players).to eq([])
+      expect(@table.dealer).to be_nil
+    end
+    it 'should add new player and dealer' do
+      # Player.new(@table)
+      Dealer.new(@table)
+      # expect(@table.players.length).to eq(1)
+      expect(@table.dealer).not_to be_nil
+    end
   end
 end
