35170ef8542d5b2145fec74d56c3f3d32f14d567
Dmitry Davdov <dmfrodol@yandex.ru>
2019-12-15 17:14:10 +0300

    add method for displaying player statistics

--- player.rb
+++ player.rb
@@ -28,6 +28,7 @@ class Player
   end
 
   def take_turn
+    display_statistics
     begin
       show_choice
       choice = process_user_input
@@ -43,6 +44,13 @@ class Player
     when 3
       action_open_hands
     end
+  end
+
+  def display_statistics
+    puts "Money: #{@money}"
+    puts "Current score: #{@score}"
+    puts "Hand: #{@hand.map(&:to_s).join(' ')}"
+    puts
   end
 
   def add_card(card)
