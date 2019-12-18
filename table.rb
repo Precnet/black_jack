e65cce000ad0b6945eef51e5e20cfe9bc8f2b66d
nodmin <dmfrodol@yandex.ru>
2019-12-18 04:20:36 -0800

    move some methods to game.rb add method for making bets

--- table.rb
+++ table.rb
@@ -32,67 +32,8 @@ class Table
     end
   end
 
-  def start_game
-    validate!
-    construct_deck
-    # ToDo
-    # make_bets
-    distribute_initial_cards
-    calculate_scores
-    display_statistics
-    #@player.take_turn
-  end
-
-  private
-
-  def validate!
-    validate_player
-    validate_dealer
-  end
-
-  def validate_player
-    message = 'There should be a player at a table!'
-    raise BlackjackError, message unless @player
-  end
-
-  def validate_dealer
-    message = 'There should be a dealer at the table!'
-    raise BlackjackError, message unless @dealer
-  end
-
-  def construct_deck
-    @deck = Deck.new
-    @deck.shuffle!
-  end
-
-  def distribute_initial_cards
-    @player.add_card(@deck.take_card)
-    @player.add_card(@deck.take_card)
-    @dealer.add_card(@deck.take_card)
-    @dealer.add_card(@deck.take_card)
-  end
-
-  def calculate_scores
-    @player.increase_score_by(calculate_score(@player.hand))
-    @dealer.increase_score_by(calculate_score(@dealer.hand))
-  end
-
-  def calculate_score(cards)
-    has_ace = cards.any? { |card| card.value == :a }
-    result = cards.map { |card| SCORES[card.value.to_s] }.sum
-    result -= 10 if result > 21 && has_ace
-    result
-  end
-
-  def display_statistics
-    puts "Money: #{@player.money}"
-    puts "Current score: #{@player.score}"
-    puts "Hand: #{@player.hand.map(&:to_s).join(' ')}"
-    puts
+  def make_bets
+    @player.make_bet
+    @dealer.make_bet
   end
 end
-
-table = Table.new
-table.add_to_table(Player.new)
-table.add_to_table(Dealer.new)
-table.start_game
