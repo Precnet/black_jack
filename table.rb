6baa43c1021761485dd6628716f71d1ece3d4a59
nodmin <dmfrodol@yandex.ru>
2019-12-18 04:49:57 -0800

    add constant for scores

--- table.rb
+++ table.rb
@@ -4,9 +4,6 @@ require_relative 'player.rb'
 require_relative 'dealer.rb'
 
 class Table
-  SCORES = { 'a' => 11, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6,
-             '7' => 7, '8' => 8, '9' => 9, '10' => 10, 'j' => 10,
-             'q' => 10, 'k' => 10 }.freeze
   attr_reader :player, :dealer, :bank
 
   def initialize
